f64ba4294b00edf0955016e34155d82df577c141
Eric Paul Michael Francis <eric.francis1103@gmail.com>
2025-05-25 14:27:07 -0500

    new files
    new files to commit.,

--- PythonCourse/contacts.py
+++ PythonCourse/contacts.py
@@ -1,2 +1,19 @@
 
+<<<<<<< Updated upstream
 contacts = {}
+=======
+contacts = {
+    'number': 4,
+    'students':
+        [
+            {'name': 'Eric Francis', 'email': 'Eric.Francis1103@gmail.com'},
+            {'name': 'Michael Garabaldi', 'email': 'Micheal.Garabaldi@gmail.com'},
+            {'name': 'Harry Potter', 'email': 'harry@example.com'},
+            {'name': 'Hermione Granger', 'email': 'hermonie@example.com'}
+        ]   
+}
+
+print('Student emails:')
+for student in contacts['students']:
+       print(student['email'])
+>>>>>>> Stashed changes
